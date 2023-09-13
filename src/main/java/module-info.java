e857ef433ae0db301e9febc27ab31c6e0c1be7fe
JasonBeach11 <119906700+JasonBeach11@users.noreply.github.com>
2023-09-13 15:49:00 -0600

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.memorization_mastery {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-    requires java.sql;
-
-    opens com.example.memorization_mastery to javafx.fxml;
-    exports com.example.memorization_mastery;
-}
